8ef17b8e309afbfa8712d8c447372b816a58c898
mpizzzle <michael.770211@gmail.com>
2018-04-08 21:27:41 +0100

    updating formatting

--- smash-census-client/localtunnel.rb
+++ smash-census-client/localtunnel.rb
@@ -3,45 +3,45 @@ require 'optparse'
 options = {:subdomain => 'smashcensus', :port => 8081}
 
 parser = OptionParser.new do|opts|
-	opts.banner = "Usage: localtunnel.rb [options]"
-	opts.on('-s', '--subdomain subdomain', 'Subdomain') do |subdomain|
-		options[:subdomain] = subdomain;
-	end
+  opts.banner = "Usage: localtunnel.rb [options]"
+  opts.on('-s', '--subdomain subdomain', 'Subdomain') do |subdomain|
+    options[:subdomain] = subdomain;
+  end
 
-	opts.on('-p', '--port port', 'Port') do |port|
-		options[:port] = port;
-	end
+  opts.on('-p', '--port port', 'Port') do |port|
+    options[:port] = port;
+  end
 
-	opts.on('-h', '--help', 'Displays Help') do
-		puts opts
-		exit
-	end
+  opts.on('-h', '--help', 'Displays Help') do
+    puts opts
+    exit
+  end
 end
 
 parser.parse!
 
 def ordinal(number)
-	abs_number = number.to_i.abs
+  abs_number = number.to_i.abs
 
-	if (11..13).include?(abs_number % 100)
-		"th"
-	else
-		case abs_number % 10
-			when 1; "st"
-			when 2; "nd"
-			when 3; "rd"
-			else    "th"
-		end
-	end
+  if (11..13).include?(abs_number % 100)
+    "th"
+  else
+    case abs_number % 10
+      when 1; "st"
+      when 2; "nd"
+      when 3; "rd"
+      else    "th"
+    end
+  end
 end
 
 def ordinalize(number)
-	"#{number}#{ordinal(number)}"
+  "#{number}#{ordinal(number)}"
 end
 
 launch_count = 0
 while true
-	launch_count += 1
-	puts "Running localtunnel for the #{ordinalize(launch_count)} time"
-	`lt --port #{options[:port]} --subdomain #{options[:subdomain]}`
+  launch_count += 1
+  puts "Running localtunnel for the #{ordinalize(launch_count)} time"
+  `lt --port #{options[:port]} --subdomain #{options[:subdomain]}`
 end
