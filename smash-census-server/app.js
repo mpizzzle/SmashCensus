337e617d6f79b868afdb632e463dd94cc085cda7
mpizzzle <michael.770211@gmail.com>
2018-04-08 20:56:35 +0100

    formatting tabs to spaces

--- smash-census-server/app.js
+++ smash-census-server/app.js
@@ -9,18 +9,11 @@ var index = require('./src/index');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', index);
 
 // catch 404 and forward to error handler
--- smash-census-server/src/index.js
+++ smash-census-server/src/index.js
@@ -7,57 +7,57 @@ var db;
 
 /* connect to the database specified above */
 mongodb.MongoClient.connect(mongourl, function(err, database) {
-	if (err) {
-		console.log(err);
-	} else {
-		db = database;
-	}
+  if (err) {
+    console.log(err);
+  } else {
+    db = database;
+  }
 });
 
 /* http GET request to retrieve all profiles in json format */
 router.get('/profiles', function(req, res, next) {
-	// Get all profiles
-	db.collection('profiles').find({}).toArray(function(err, result) {
-		if (err) {
-			res.send(err);
-		} else if (result.length) {
-			res.json(result);
-		} else {
-			res.send('No profiles found!');
-		}
-	});
+  // Get all profiles
+  db.collection('profiles').find({}).toArray(function(err, result) {
+    if (err) {
+      res.send(err);
+    } else if (result.length) {
+      res.json(result);
+    } else {
+      res.send('No profiles found!');
+    }
+  });
 });
 
 /* http POST request to add a profile, request should be in json format */
 router.post('/postprofile', function(req, res) {
-	// Get the profile passed from the form
-	var p = {profile: req.body.profile, firstName: req.body.firstName,
-		surname: req.body.surname, tag: req.body.tag, region: req.body.region,
-		main: req.body.main, secondary: req.body.secondary};
+  // Get the profile passed from the form
+  var p = {profile: req.body.profile, firstName: req.body.firstName,
+    surname: req.body.surname, tag: req.body.tag, region: req.body.region,
+    main: req.body.main, secondary: req.body.secondary};
 
-	// Insert the profile into the database
-	db.collection('profiles').insert([p], function (err, result) {
-		if (err) {
-			console.log(err);
-		} else {
-			// Redirect to the updated profile list
-			res.redirect("profiles");
-		}
-	});
+  // Insert the profile into the database
+  db.collection('profiles').insert([p], function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      // Redirect to the updated profile list
+      res.redirect("profiles");
+    }
+  });
 });
 
 /* http POST request to delete a profile, request should be in json format */
 router.post('/deleteprofile', function(req, res) {
-	var query = { tag: req.body.tag };
+  var query = { tag: req.body.tag };
 
-	// Delete profile that matches tag
-	db.collection('profiles').deleteOne(query, function(err, obj) {
-		if (err) {
-			console.log(err);
-		} else {
-			res.redirect('profiles');
-		}
-	});
+  // Delete profile that matches tag
+  db.collection('profiles').deleteOne(query, function(err, obj) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('profiles');
+    }
+  });
 });
 
 module.exports = router;
