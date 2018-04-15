7678ef12c1371c4cc8d8e9ab2845cd4c23072782
Stan-Sasquatch <stephenppercival@gmail.com>
2018-04-15 13:43:49 +0100

    hello, new commit

--- smash-census-client/src/components/Profile.js
+++ smash-census-client/src/components/Profile.js
@@ -7,7 +7,7 @@ export default class Profile extends React.Component {
     return (
       <div class="col-md-4">
         <h4>{title}</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
+        <p>consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
         <a class="btn btn-default" href="#">More Info</a>
       </div>
     );
